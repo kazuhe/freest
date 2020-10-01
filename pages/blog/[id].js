4f26598e8fd0d3806ac4440417627dae8b98a5cd
Kazuhiro Ohara <oohara.k@tam-tam.co.jp>
2020-10-01 16:51:44 +0900

    add : Get category

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -1,10 +1,11 @@
 import styles from '../../styles/Home.module.scss'
 
-export default function BlogId({blog}) {
+export default function BlogId({ blog }) {
   return (
     <main className={styles.main}>
       <h1 className={styles.title}>{blog.title}</h1>
       <p className={styles.publishedAt}>{blog.publishedAt}</p>
+      <p className={styles.category}>{blog.category && `${blog.category.name}`}</p>
       <div
         dangerouslySetInnerHTML={{
           __html: `${blog.body}`,
